f791489661d98f2a6f779d65766c0f96d7819720
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-01-14 16:14:44 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,6 +143,12 @@ var auditTask = function(taskEl) {
   }
 };
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
 // value of due date was changed
 $(".list-group").on("change", "input[type='text']", function () {
   // get current text
@@ -183,16 +189,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in 
@@ -231,8 +239,15 @@ $(".card .list-group").sortable({
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
+  over: function() {
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function() {
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   drop: function (event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -255,7 +270,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
